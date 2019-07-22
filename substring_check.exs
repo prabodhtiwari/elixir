7633a2efa268b25a7079840cbe10b6c8d0d455eb
Prabodh <prabodhtiwari@outlook.com>
2019-07-22 23:48:09 +0530

    imporved substring check

--- substring_check.exs
+++ substring_check.exs
@@ -4,10 +4,11 @@ defmodule SubStringCheck do
   """
 
   def check do
-    a = IO.gets("String 1: ")
-    b = IO.gets("String 2: ")
+    a = IO.gets("String 1: ") |> String.trim
+    b = IO.gets("String 2: ") |> String.trim
 
-    if String.contains?(String.trim(a), String.trim(b)) do
+    #if String.contains?(String.trim(a), String.trim(b)) do
+    if Regex.match?(~r/#{b}/, a) do
       IO.puts "String 2 is subset of string 1."
     else
       IO.puts "String 2 is not a subset of string 1."
