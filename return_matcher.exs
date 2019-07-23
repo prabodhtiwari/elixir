df4be078645d540fc1bac0517569662dae416d68
Prabodh <prabodhtiwari@outlook.com>
2019-07-23 19:28:59 +0530

    convert a number to a string, the contents of which depend on the number's factors

--- return_matcher.exs
+++ return_matcher.exs
@@ -6,7 +6,8 @@ defmodule ReturnMatches do
   def check do
     a = IO.gets("String 1: ") |> String.trim
     b = IO.gets("String 2: ") |> String.trim
-    Regex.scan(~r/#{b}/, a)
+    Regex.scan(~r/?#{b}?/, a)
+    |> Enum.map(fn x -> IO.puts x end)
   end
 end
 
