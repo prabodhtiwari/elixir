794fbbcf09cbf4b3cd3aed0e26818c56fd445d43
Prabodh <prabodhtiwari@outlook.com>
2019-07-24 16:59:15 +0530

    Count the number of words in the sentence.

--- word_count.exs
+++ word_count.exs
@@ -1,22 +1,32 @@
-
 defmodule Words do
+  @doc """
+  Count the number of words in the sentence.
 
-  @spec count(String.t) :: map()
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t()) :: map
   def count(sentence) do
     sentence
-    |> split
-    |> count_words
+    |> _remove
+    |> _lower_case
+    |> _split
+    |> _count_words
   end
 
-  defp split(sentence), do: String.split(sentence, " " )
+  defp _remove(sentence), do: String.replace(sentence, ~r/[^-_a-zA-Z0-9äöüÄÖÜ ]/, "")
 
-  defp count_words(words) do
-    Enum.reduce(words, Map.new, &count_word/2)
+  defp _lower_case(sentence), do:  String.downcase(sentence)
+
+  defp _split(sentence), do: String.split(sentence, [" ", "_"], trim: true )
+
+
+  defp _count_words(words) do
+    Enum.reduce(words, Map.new, &_count_word/2)
   end
 
-  defp count_word(word, words) do
-    lower_word = String.downcase(word)
-    Map.update(words, lower_word, 1, fn(value) -> value + 1 end)
+  defp _count_word(word, words) do
+    Map.update(words, word, 1, fn(value) -> value + 1 end)
   end
+
+
 end
-
