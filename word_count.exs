825b6c18503662189b89077f7181a616e6844ead
Prabodh <prabodhtiwari@outlook.com>
2019-07-25 22:51:22 +0530

    Improved regex in count the number of words in the sentence.

--- word_count.exs
+++ word_count.exs
@@ -8,16 +8,13 @@ defmodule Words do
   def count(sentence) do
     sentence
     |> lower_case()
-    |> remove()
     |> split()
     |> count_words()
   end
 
   defp lower_case(sentence), do:  String.downcase(sentence)
 
-  defp remove(sentence), do: String.replace(sentence, ~r/([^\w\- ])/u, "")
-
-  defp split(sentence), do: String.split(sentence, ~r/[_ ]/, trim: true )
+  defp split(sentence), do: String.split(sentence, ~r/([^\w\-]| |_)/u, trim: true )
 
   defp count_words(words), do: Enum.reduce(words, Map.new, &count_word/2)
   defp count_word(word, words), do: Map.update(words, word, 1, fn(value) -> value + 1 end)
