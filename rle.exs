421e559f8494cbdcc077fac2e6afbb82fd3c208b
Prabodh <prabodhtiwari@outlook.com>
2019-08-03 23:12:48 +0530

    Implemented run-length encoding and decoding.

---
+++ rle.exs
@@ -0,0 +1,21 @@
+defmodule RunLengthEncoder do
+  @doc """
+  Generates a string where consecutive elements are represented as a data value and count.
+  "AABBBCCCC" => "2A3B4C"
+  For this example, assume all input are strings, that are all uppercase letters.
+  It should also be able to reconstruct the data into its original form.
+  "2A3B4C" => "AABBBCCCC"
+  """
+  @spec encode(String.t) :: String.t
+  def encode(string) do
+    Regex.replace(~r/(.)\1*/, string, &(encode String.length(&1), &2))
+  end
+
+  defp encode(1, char), do: char
+  defp encode(len, char), do: Integer.to_string(len) <> char
+
+  @spec decode(String.t) :: String.t
+  def decode(string) do
+    Regex.replace(~r/(\d+)(.)/, string, fn(_, len, char) -> String.duplicate(char,String.to_integer(len)) end)
+  end
+end
